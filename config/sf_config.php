60cf5940f771d1f0e3f2eea324b159fcfa287fa1
erdem.akbulut <erdem.akbulut@teknofix.com.tr>
2021-10-12 00:46:05 +0300

    fix

--- config/sf_config.php
+++ config/sf_config.php
@@ -4,6 +4,7 @@ return
 [
     'log_directory' => 'logs/Salesforce/Connections',
     'wsdl_path' => 'enterprise.xml',
+    'location' => env('SALESFORCE_LOCATION'),
     'username' => env('SALESFORCE_USERNAME'),
     'password' => env('SALESFORCE_PASSWORD'),
     'token' => env('SALESFORCE_TOKEN'),
