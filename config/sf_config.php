4e87c6311c8df31958d1f89ee70fdee480023509
erdem.akbulut <erdem.akbulut@teknofix.com.tr>
2021-10-12 00:21:08 +0300

    regenerate sf session

--- config/sf_config.php
+++ config/sf_config.php
@@ -4,11 +4,7 @@ return
 [
     'log_directory' => 'logs/Salesforce/Connections',
     'wsdl_path' => 'enterprise.xml',
-    'current' =>
-    [
-        'username' => env('SALESFORCE_USERNAME'),
-        'password' => env('SALESFORCE_PASSWORD'),
-        'token' => env('SALESFORCE_TOKEN'),
-        'user_type' => env('SALESFORCE_USER_TYPE') ?? 'current',
-    ]
+    'username' => env('SALESFORCE_USERNAME'),
+    'password' => env('SALESFORCE_PASSWORD'),
+    'token' => env('SALESFORCE_TOKEN'),
 ];
